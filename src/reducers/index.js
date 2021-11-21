55436717cf8585d4d274b137cd633a92e6320ee7
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-11-20 20:27:37 -0500

    constructed reducer function

---
+++ src/reducers/index.js
@@ -0,0 +1,36 @@
+const initialState = {
+    getPlayers: [],
+    isFetching: false,
+    error: ''
+}
+
+//   axios
+  //     .get('https://www.balldontlie.io/api/v1/players')
+  //     .then(res => console.log("response", res))
+  //     .catch(err => console.log("errors", err));
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case('FETCH_PLAYER_LOADING'):
+            return({
+                ...state,
+                isFetching: true
+            });
+        case('FETCH_PLAYER_SUCCESS'):
+            return({
+                ...state,
+                getPlayers: action.payload,
+                isFetching: false
+            });
+        case('FETCH_PLAYER_FAIL'):
+            return({
+                ...state,
+                isFetching: false,
+                error: action.payload
+            })
+        default:
+            return state;
+    }
+}
+
+export default reducer;
